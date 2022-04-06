0129288499b73f7e18113d15874bf3da7b30c7e1
Álvaro Sierra <asierraperez@danielcastelao.org>
2022-04-06 17:11:38 +0200

    separado en funciones

--- CON_apiKey/main.js
+++ CON_apiKey/main.js
@@ -1,36 +1,45 @@
+function borraContenidoHTML(e_content) {
+    if (e_content) {
+        var contenido = document.getElementsByClassName("contenido")
+        var elementos = contenido.length
+
+        for (let i = 0; i < elementos; i++) {
+            console.log(contenido)
+            document.getElementById("content").removeChild(contenido[0])
+
+        }
+    }
+}
+
+function fechaAleatoria() {
+    var hoy = new Date()
+    var max_year = hoy.getFullYear() + 1
+    var year = Math.floor(Math.random() * (max_year - 2000) + 2000)
+    var month = Math.floor(Math.random() * (13 - 1) + 1)
+    if (month == 2) {
+        var day = Math.floor(Math.random() * (30 - 1) + 1)
+    } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        var day = Math.floor(Math.random() * (32 - 1) + 1)
+    } else {
+        var day = Math.floor(Math.random() * (31 - 1) + 1)
+    }
+    return year + "-" + month + "-" + day
+}
+
+
+
 function __main__() {
+
     var search = document.getElementById("btn")
     var api_key = "eGcz4CusU7ChsTgFvaJlIFN58h57e01iDGARwcfe"
     var existe_content = false
+
+
+
     search.addEventListener("click", (evt) => {
 
-
-        if (existe_content) {
-            var contenido = document.getElementsByClassName("contenido")
-            var elementos = contenido.length
-
-            for (let i = 0; i < elementos; i++) {
-                console.log(contenido)
-                document.getElementById("content").removeChild(contenido[0])
-
-            }
-        }
-
-
-
-        var hoy = new Date()
-        var max_year = hoy.getFullYear() + 1
-        var year = Math.floor(Math.random() * (max_year - 2000) + 2000)
-        var month = Math.floor(Math.random() * (13 - 1) + 1)
-        if (month == 2) {
-            var day = Math.floor(Math.random() * (30 - 1) + 1)
-        } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-            var day = Math.floor(Math.random() * (32 - 1) + 1)
-        } else {
-            var day = Math.floor(Math.random() * (31 - 1) + 1)
-        }
-        var date = year + "-" + month + "-" + day
-        console.log(date)
+        borraContenidoHTML(existe_content)
+        var date = fechaAleatoria()
 
         var settings = {
             "async": true,
